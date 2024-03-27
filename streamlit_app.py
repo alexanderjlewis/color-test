4870069c62f5b0889380c50e7ecd44963f50f1d5
Alex Lewis <30378663+alexanderjlewis@users.noreply.github.com>
2024-03-27 22:53:18 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,16 +3,27 @@ from PIL import Image
 import cv2
 from colorblind import colorblind
 
-st.title("Colour Bling Simulator")
+st.title("Colour Blindness Simulator")
 
 uploaded_img = st.file_uploader("Upload Image")
 
 if uploaded_img is not None:
+    st.title("Original Image")
     img = Image.open(uploaded_img)
     
-    st.image(img, "Uploaded image")
+    
     
     # simulate protanopia
     simulated_img = colorblind.simulate_colorblindness(img, colorblind_type='protanopia')
 
-    st.image(simulated_img, "Uploaded image")
+    st.image(simulated_img, "Simulated Protanopia")
+
+    # simulate tritanopia
+    simulated_img = colorblind.simulate_colorblindness(img, colorblind_type='tritanopia')
+
+    st.image(simulated_img, "Simulated Tritanopia")
+
+    # simulate deuteranopia
+    simulated_img = colorblind.simulate_colorblindness(img, colorblind_type='deuteranopia')
+
+    st.image(simulated_img, "Simulated Deuteranopia")
